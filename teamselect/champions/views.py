1371b5906bb55e43f5733c823749ce42090ca6b2
smelnicki <smelnicki3@gmail.com>
2012-12-11 21:42:42 -0600

    Forgot to include some files.

---
+++ teamselect/champions/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.http import Http404
+from django.contrib.auth.models import User
+from models import Champion
+# from django.contrib.auth import authenticate, login
+from django.utils import sinplejson

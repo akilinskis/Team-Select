fe6a1387355e4464888c38f5755a26530fad47dc
akilinskis <andrewkilinskis@gmail.com>
2012-12-13 16:57:11 -0600

    I'll never hold local fiels again. Fk merging, maaaaaan.

--- players/models.py
+++ players/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 # Champions Model
 # Defines the Champions table
-class Player( models.Model ):
-    name = models.CharField( "summoner name", max_length=32)
+# Proper weight usage would have all fields sum to 100
+class Player(models.Model):
+    name = models.CharField("summoner name", max_length=32)
+    top = models.IntegerField("top lane position weight")
+    jungle = models.IntegerField("jungle position weight")
+    mid = models.IntegerField("mid lane position weight")
+    adc = models.IntegerField("ad carry position weight")
+    support = models.IntegerField("support position weight")
+    counters = models.ManyToManyField(Champion)
+    attributes = models.ManyToManyField(Attribute)
     
     def __unicode__(self):
         return self.name

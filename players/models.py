009862c456544c2b3474bf6d12ca1615f6b052b7
akilinskis <andrewkilinskis@gmail.com>
2012-12-13 20:06:39 -0600

    Fixed the players stuff I messed up... sorry Steeeeeeve.

--- players/models.py
+++ players/models.py
@@ -2,16 +2,9 @@ from django.db import models
 
 # Champions Model
 # Defines the Champions table
-# Proper weight usage would have all fields sum to 100
 class Player(models.Model):
     name = models.CharField("summoner name", max_length=32)
-    top = models.IntegerField("top lane position weight")
-    jungle = models.IntegerField("jungle position weight")
-    mid = models.IntegerField("mid lane position weight")
-    adc = models.IntegerField("ad carry position weight")
-    support = models.IntegerField("support position weight")
-    counters = models.ManyToManyField(Champion)
-    attributes = models.ManyToManyField(Attribute)
+    server = models.CharField("server", max_length=8)
     
     def __unicode__(self):
-        return self.name
+        return self.name + " [" + self.server + "]"
